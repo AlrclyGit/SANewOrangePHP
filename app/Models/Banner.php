16655fed2a63b2bb6af878c01fa0c25058657c01
AlrclySuzuki <Alrcly@foxmail.com>
2020-08-25 21:38:31 +0800

    创建「Banner模型」和「Banner迁移文件」，并实现 Banner 模型「通过ID获取Banner」、「 关联BannerItem」的方法。

---
+++ app/Models/Banner.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+class Banner extends BaseModel
+{
+
+    // 隐藏字段
+    protected $hidden = ['created_at','updated_at','deleted_at'];
+
+    /*
+     * 关联BannerItem
+     */
+    public function items()
+    {
+        return $this->hasMany(BannerItem::class);
+    }
+
+    /*
+     * 通过ID获取Banner
+     */
+    static public function getBannerByID($id)
+    {
+        return self::with('items.img')
+            ->where('id', '=', $id)
+            ->get();
+    }
+}
---
+++ database/migrations/2020_08_25_085829_create_banners_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBannersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('banners', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->comment('轮播块名');
+            $table->string('description')->comment('详情');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('banners');
+    }
+}
