d30aaa80e66e06cf662365a3a947a7d8c5debdeb
AlrclySuzuki <Alrcly@foxmail.com>
2020-08-25 21:28:46 +0800

    创建图片路径的枚举类

---
+++ app/Enums/ImagePath.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Name:
+ * User: 萧俊介
+ * Date: 2020/8/25
+ * Time: 8:29 下午
+ * Created by SANewOrangePHP制作委员会.
+ */
+
+namespace App\Enums;
+
+
+class ImagePath
+{
+
+    // 图片地址类型
+    const localHost = 1;
+    const oss = 2;
+
+    // 图片地址URL
+    const localHostPath = 'https://al.alrcly.com/image/';
+    const ossPath = '';
+
+}
