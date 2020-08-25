6cd602795b6c8850325e581dfad49e9bcdaed308
AlrclySuzuki <Alrcly@foxmail.com>
2020-08-25 21:39:12 +0800

    创建「BannerItem模型」和「BannerItem迁移文件」，并实现 BannerItem 模型「 关联img」的方法。

---
+++ app/Models/BannerItem.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+class BannerItem extends BaseModel
+{
+
+    // 隐藏字段
+    protected $hidden = ['id','img_id','banner_id','created_at','updated_at','deleted_at'];
+
+
+    /*
+     * 关联img
+     */
+    public function img()
+    {
+        return $this->belongsTo(Image::class);
+    }
+}
