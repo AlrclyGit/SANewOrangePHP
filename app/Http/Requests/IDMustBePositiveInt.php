9bd7ec4115f408f908c68dba7cc820a76d355b5a
AlrclySuzuki <Alrcly@foxmail.com>
2020-08-25 21:33:01 +0800

    创建验证「ID为正整数」的验证器

---
+++ app/Http/Requests/IDMustBePositiveInt.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Name: ID是否为正整数
+ * User: 萧俊介
+ * Date: 2020/8/25
+ * Time: 11:28 上午
+ * Created by SANewOrangePHP制作委员会.
+ */
+
+namespace App\Http\Requests;
+
+
+use App\Rules\IsPositiveInteger;
+use Illuminate\Foundation\Http\FormRequest;
+
+class IDMustBePositiveInt extends FormRequest
+{
+
+    /**
+     * 权限判断
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * 过滤规则
+     */
+    public function rules()
+    {
+        return [
+            'id' => ['required',new IsPositiveInteger]
+        ];
+    }
+
+    /**
+     * 错误消息
+     */
+    public function messages()
+    {
+        return[
+            'id.required' => 'ID不能为空'
+        ];
+    }
+
+}
---
+++ app/Rules/IsPositiveInteger.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Name: 值是否为正整数
+ * User: 萧俊介
+ * Date: 2020/8/25
+ * Time: 11:34 上午
+ * Created by SANewOrangePHP制作委员会.
+ */
+
+namespace App\Rules;
+
+
+use Illuminate\Contracts\Validation\Rule;
+
+class IsPositiveInteger implements Rule
+{
+
+    /**
+     * 构造方法
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * 验证通过条件定义
+     */
+    public function passes($attribute, $value)
+    {
+        if (is_numeric($value) && is_int($value + 0) && ($value + 0) > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 自定义字段名
+     */
+    public function attributes()
+    {
+        return [
+            'id' => 'ID'
+        ];
+    }
+
+    /**
+     * 错误消息
+     */
+    public function message()
+    {
+        return ':attribute必须为正整数';
+    }
+}
